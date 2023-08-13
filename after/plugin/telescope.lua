51ee53bf00cf18a58a78048143f3c8a26fe59afc
lk <leonid.kogan@gmail.com>
2023-08-13 20:42:42 +0300

    find_files show hidden files

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,18 @@
+
+
+require("telescope").setup {
+	defaults = {
+		find_files = {
+			hidden = true
+		},
+	},
+	pickers = {
+		find_files = {
+			hidden=true,
+		},
+	},
+}
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fo', builtin.oldfiles, {})
@@ -8,4 +23,5 @@ vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>pf',  function() 
 	builtin.grep_string({ search = vim.fn.input("grep > ") });
 end)
+
 
